d9a5f123339eea0850e01561a8d4a13bbc7568e6
rizkym4l <115598877+rizkym4l@users.noreply.github.com>
2026-07-13 18:04:32 +0700

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,11 @@
+from functools import reduce
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        duplicate = False
+        nums.sort()
+        for i,num in enumerate(nums):
+            if i != len(nums) -1:
+                if nums[i] == nums[i+1]:
+                    duplicate = True       
+        return duplicate
